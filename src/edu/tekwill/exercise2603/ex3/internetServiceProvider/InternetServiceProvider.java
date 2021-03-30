720049e1e3fcc4d861a6b3f582bbdc665d6dd511
Ivan Padureac <padureacion@gmail.com>
2021-03-30 22:08:46 +0300

    modify ex3. homework 2603

--- src/edu/tekwill/exercise2603/ex3/internetServiceProvider/InternetServiceProvider.java
+++ src/edu/tekwill/exercise2603/ex3/internetServiceProvider/InternetServiceProvider.java
@@ -17,22 +17,17 @@ public class InternetServiceProvider {
                 billPrice = userNumberOfHours <= 10
                         ? 9.95f
                         : 9.95f + ((userNumberOfHours - 10) * 2);
-            }
-            if (userNumberOfHours > 0) {
-                if (userPackage.equalsIgnoreCase("B")) {
-                    billPrice = userNumberOfHours <= 20
-                            ? 13.95f
-                            : 13.95f + ((userNumberOfHours - 20) * 1);
+            } else if (userPackage.equalsIgnoreCase("B")) {
+                billPrice = userNumberOfHours <= 20
+                        ? 13.95f
+                        : 13.95f + ((userNumberOfHours - 20) * 1);
 
-                } else if (userPackage.equalsIgnoreCase("C")) {
-                    billPrice = 19.95f;
-                }
-                if (billPrice > 0) {
-                    System.out.println("Total charges for package you chose " + userPackage.toUpperCase() + " is: " + billPrice);
-                } else {
-                    System.out.println("You must provide a positive number of hours or a valid internet package");
-                }
+            } else if (userPackage.equalsIgnoreCase("C")) {
+                billPrice = 19.95f;
             }
         }
+        if (billPrice > 0) {
+            System.out.println("Total charges for package you chose " + userPackage.toUpperCase() + " is: " + billPrice);
+        } else System.out.println("You must provide a positive number of hours or a valid internet package");
     }
 }
